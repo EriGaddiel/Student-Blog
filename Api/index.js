2f7ad2ce98b7d42b81091d31fece379f57468566
Darael Eri Gaddiel <daraeleri@gmail.com>
2024-05-02 18:21:48 +0100

    Creating and connecting mongoDB

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const port = 5000
+
+mongoose.connect(process.env.MONGO_URL).then(() => {console.log("Connected to mongoDB")})
 
 app.listen(port, ()=>{
     console.log(`Server is running in port: ${ port }`)
