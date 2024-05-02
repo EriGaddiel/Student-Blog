d679d89e1dd243ef9a1e69a7e805238f3d3c4162
Darael Eri Gaddiel <daraeleri@gmail.com>
2024-05-02 17:47:20 +0100

    Initailizing react project and tailwindcss, creating routes and pages and navigation header

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Header from './Components/Header'
+
+function App() {
+  return (
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App 
